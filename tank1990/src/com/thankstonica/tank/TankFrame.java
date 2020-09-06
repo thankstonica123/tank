196bda512020efcece98e0c973047725ae707878
thankstonica123 <thankstonica@126.com>
2020-09-06 10:41:52 +0800

    create a TankFrame.java

---
+++ tank1990/src/com/thankstonica/tank/TankFrame.java
@@ -0,0 +1,10 @@
+package com.thankstonica.tank;
+
+/**
+ * Created by thankstonica on 2020/9/6.
+ */
+public class TankFrame {
+    public static void main(String[] args) {
+        System.out.println("hello tank1990");
+    }
+}
