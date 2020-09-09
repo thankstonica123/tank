ce3d12446e33fdb95c602a57657146f81438d2d6
thankstonica123 <thankstonica@126.com>
2020-09-10 00:57:42 +0800

    从frame继承，画出图形

---
+++ tank1990/src/com/thankstonica/tank/T.java
@@ -0,0 +1,10 @@
+package com.thankstonica.tank;
+
+/**
+ * Created by thankstonica on 2020/9/10.
+ */
+public class T {
+    public static void main(String[] args) {
+        new TankFrame();
+    }
+}
--- tank1990/src/com/thankstonica/tank/TankFrame.java
+++ tank1990/src/com/thankstonica/tank/TankFrame.java
@@ -1,10 +1,29 @@
 package com.thankstonica.tank;
 
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
 /**
  * Created by thankstonica on 2020/9/6.
  */
-public class TankFrame {
-    public static void main(String[] args) {
-        System.out.println("hello tank1990");
+public class TankFrame extends Frame{
+
+    public TankFrame(){
+        this.setSize(800,600);
+        this.setTitle("TankWar1990");
+        this.setResizable(false);
+        this.setVisible(true);
+        this.addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                System.exit(0);
+            }
+        });
+    }
+
+    @Override
+    public void paint(Graphics g) {
+        g.fill3DRect(200,200,50,50,false);
     }
 }
