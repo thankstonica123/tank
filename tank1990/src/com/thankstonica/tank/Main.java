7e64848c906a50ed980bfadc804c27e9b6b25e7b
thankstonica123 <thankstonica@126.com>
2020-09-10 12:34:38 +0800

    添加键盘监听

--- tank1990/src/com/thankstonica/tank/Main.java
+++ tank1990/src/com/thankstonica/tank/Main.java
@@ -3,7 +3,7 @@ package com.thankstonica.tank;
 /**
  * Created by thankstonica on 2020/9/10.
  */
-public class T {
+public class Main {
     public static void main(String[] args) {
         new TankFrame();
     }
--- tank1990/src/com/thankstonica/tank/TankFrame.java
+++ tank1990/src/com/thankstonica/tank/TankFrame.java
@@ -1,6 +1,8 @@
 package com.thankstonica.tank;
 
 import java.awt.*;
+import java.awt.event.KeyAdapter;
+import java.awt.event.KeyEvent;
 import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 
@@ -8,6 +10,8 @@ import java.awt.event.WindowEvent;
  * Created by thankstonica on 2020/9/6.
  */
 public class TankFrame extends Frame{
+
+    int x = 200,y = 200;
 
     public TankFrame(){
         this.setSize(800,600);
@@ -20,10 +24,26 @@ public class TankFrame extends Frame{
                 System.exit(0);
             }
         });
+        this.addKeyListener(new MyKeyListener());
     }
 
     @Override
     public void paint(Graphics g) {
-        g.fill3DRect(200,200,50,50,false);
+        g.fill3DRect(x,y,50,50,false);
+        x += 100;
+        y += 100;
+        System.out.println("paint ...");
+    }
+
+    class MyKeyListener extends KeyAdapter{
+        @Override
+        public void keyPressed(KeyEvent e) {
+            System.out.println("key pressed ...");
+        }
+
+        @Override
+        public void keyReleased(KeyEvent e) {
+            System.out.println("key released ..");
+        }
     }
 }
