fee4059c3168c5b04324c5c30ea042fb4f890e23
thankstonica123 <thankstonica@126.com>
2020-09-10 22:52:52 +0800

    添加方向枚举

---
+++ tank1990/src/com/thankstonica/tank/Direction.java
@@ -0,0 +1,8 @@
+package com.thankstonica.tank;
+
+/**
+ * Created by thankstonica on 2020/9/10.
+ */
+public enum Direction {
+    LEFT,UP,RIGHT,DOWN
+}
--- tank1990/src/com/thankstonica/tank/TankFrame.java
+++ tank1990/src/com/thankstonica/tank/TankFrame.java
@@ -11,7 +11,14 @@ import java.awt.event.WindowEvent;
  */
 public class TankFrame extends Frame{
 
+    // 坦克初始位置
     int x = 200,y = 200;
+
+    // 坦克速度
+    public static final int SPEED = 10;
+
+    // 坦克方向
+    Direction dir = Direction.DOWN;
 
     public TankFrame(){
         this.setSize(800,600);
@@ -30,20 +37,59 @@ public class TankFrame extends Frame{
     @Override
     public void paint(Graphics g) {
         g.fill3DRect(x,y,50,50,false);
-        System.out.println("paint ...");
-        x += 5;
+        switch (dir){
+            case RIGHT:
+                x += SPEED;
+                break;
+            case DOWN:
+                y += SPEED;
+                break;
+            case LEFT:
+                x -= SPEED;
+                break;
+            case UP:
+                y -= SPEED;
+                break;
+        }
+
     }
 
     class MyKeyListener extends KeyAdapter{
+
+        boolean bR = false;
+        boolean bD = false;
+        boolean bL = false;
+        boolean bU = false;
+
         @Override
         public void keyPressed(KeyEvent e) {
-            //x += 2;
-            //repaint();
+            int keyCode = e.getKeyCode();
+            switch (keyCode){
+                case KeyEvent.VK_RIGHT:bR = true;break;
+                case KeyEvent.VK_DOWN:bD = true;break;
+                case KeyEvent.VK_LEFT:bL = true;break;
+                case KeyEvent.VK_UP:bU= true;break;
+            }
+            setMainTankDir();
         }
 
         @Override
         public void keyReleased(KeyEvent e) {
-            System.out.println("key released ..");
+            int keyCode = e.getKeyCode();
+            switch (keyCode){
+                case KeyEvent.VK_RIGHT:bR = false;break;
+                case KeyEvent.VK_DOWN:bD = false;break;
+                case KeyEvent.VK_LEFT:bL = false;break;
+                case KeyEvent.VK_UP:bU= false;break;
+            }
+            setMainTankDir();
+        }
+
+        private void setMainTankDir() {
+            if(bR){dir = Direction.RIGHT;}
+            if(bD){dir = Direction.DOWN;}
+            if(bL){dir = Direction.LEFT;}
+            if(bU){dir = Direction.UP;}
         }
     }
 }
